9fb9b188c11f9733181f033b135068bebe89066f
Алексей Леонтьев <leontev.leha06@bk.ru>
2024-12-26 10:46:06 +0500

    Подключили БД

---
+++ Pr3/ProductType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pr3;
+
+public partial class ProductType
+{
+    public int Id { get; set; }
+
+    public string TypeOfProduct { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ Pr3/TypesOfPartner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pr3;
+
+public partial class TypesOfPartner
+{
+    public int Id { get; set; }
+
+    public string TypeOfPartner { get; set; } = null!;
+
+    public virtual ICollection<Partner> Partners { get; set; } = new List<Partner>();
+}
